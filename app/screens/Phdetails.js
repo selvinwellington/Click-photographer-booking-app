0575b2e690907109cf49282e60d2772a18cba272
Selvin Wellington <selvinwellington.23it@licet.ac.in>
2022-06-03 12:38:57 +0530

    Bottom sheet changes

--- app/screens/Phdetails.js
+++ app/screens/Phdetails.js
@@ -1,34 +1,73 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 import React from "react";
 import COLORS from "../conts/colors";
 import { FloatingAction } from "react-native-floating-action";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Phdetails({ navigation }) {
   return (
-    <View style={styles.logoContainer}>
-      <Image
-        style={styles.logo}
-        source={require("../assets/studio2.webp")}
-      ></Image>
-      <Text style={{ fontWeight: "bold", color: COLORS.grey, padding: 15 }}>
-        My Works
-      </Text>
-      {/* <Text>Floating Action example</Text>  */}
-      
-      <FloatingAction
-        style={{ position: "absolute", bottom: 10, right: 10 }}
-        actions={actions}
-        onPressItem={(name) => {
-          console.log(`selected button: ${name}`);
-          navigation.navigate(name);
-        }}
-      />
-      {/* <FloatingAction
+    <SafeAreaView>
+      <View>
+        <Image
+          style={styles.logo}
+          source={require("../assets/studio2.webp")}
+        ></Image>
+        <Text style={{ fontWeight: "bold", color: COLORS.grey, padding: 15 }}>
+          My Works
+        </Text>
+        {/* <Text>Floating Action example</Text>  */}
+        <View
+          style={{
+            // backgroundColor: "black",
+            position: "relative",
+            top: 140,
+            right: 5,
+            // marginTop:30
+          }}
+        >
+          <FloatingAction
+            // style={{ position: "absolute", bottom: 10, right: 10 }}
+            actions={actions}
+            onPressItem={(name) => {
+              console.log(`selected button: ${name}`);
+              navigation.navigate(name);
+            }}
+          />
+        </View>
+        {/* <FloatingAction
   ref={(ref) => { this.floatingAction = ref; }}
   actions={[...]}
   ...
 /> */}
-    </View>
+        <View>
+          <View style={{ marginLeft: 20 }}>
+            <Text>Name</Text>
+            <Text>Email</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.userInfoSection}>
+        <View style={styles.row}>
+          <Icon name="map-marker-radius" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            Chennai, India
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="phone" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            +91-900000009
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="email" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            ricozack@email.com
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 const actions = [
@@ -88,13 +127,28 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 50,
   },
 
-  float:{
-    // flex: 1,
-    // alignItems: "center",
-    // justifyContent: "center",
-    // flex: 1,
-    // position: "absolute",
-    // bottom: 10,
-    // right: 10,
+  // float:{
+  //   // flex: 1,
+  //   position: "absolute",
+  //   bottom: 10,
+  //   right: 10,
+  // },
+
+  row: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: "500",
   },
 });
